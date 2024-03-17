e36c056374e30be32f13301a6e65abf68abf6238
youri soposki <youri.soposki@gmail.com>
2024-03-17 14:32:27 +0100

    advancing on models

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,7 @@
 class Skill < ApplicationRecord
+  belongs_to :user, optional: true
+
+  validates :name, presence: true
+  validates :level, inclusion: { in: %w(beginner intermediate advanced expert) }
+  validates :section, inclusion: { in: %w(soft hard) }
 end
