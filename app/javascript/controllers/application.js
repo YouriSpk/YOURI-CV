9bd85a112a20b3ffabb3d9163bcb00236ad6eace
youri soposki <youri.soposki@gmail.com>
2024-03-25 16:14:23 +0100

    progressing

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,12 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// javascript/packs/application.js
+
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+const application = Application.start();
+const context = require.context("../controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
