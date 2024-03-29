1e44e7f6722b99346de9039644e29e056e634010
youri soposki <youri.soposki@gmail.com>
2024-03-29 18:21:08 +0100

    registration fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validate :valid_email_for_registration
+  validate :valid_email_for_registration, on: :create
 
   private
 
   def valid_email_for_registration
     unless email == 'youri.soposki@gmail.com'
-      errors.add(:email, 'is not allowed to register.')
+      errors.add(:base, 'Email is not allowed to register.')
     end
   end
 end
