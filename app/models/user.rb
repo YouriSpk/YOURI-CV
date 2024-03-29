585be4b8124bdffbd6f9f983096ec914a2e82779
youri soposki <youri.soposki@gmail.com>
2024-03-29 17:06:22 +0100

    registration security

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :valid_email_for_registration
+
+  private
+
+  def valid_email_for_registration
+    unless email == 'youri.soposki@gmail.com'
+      errors.add(:email, 'is not allowed to register.')
+    end
+  end
 end
