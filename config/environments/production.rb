702ef666dd44a417031c75f5707613e2c651d569
youri soposki <youri.soposki@gmail.com>
2024-03-29 16:05:45 +0100

    fixing issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,79 +3,85 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Force HTTPS on production
-  config.force_ssl = true
-
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  config.active_job.queue_adapter     = :sidekiq
+  # In the development environment your application's code is reloaded any time
+  # it changes. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.enable_reloading = true
 
   # Do not eager load code on boot.
-  config.eager_load = true
+  config.eager_load = false
 
-  # Enable caching
-  config.action_controller.perform_caching = true
-  config.cache_store = :memory_store
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{2.days.to_i}"
-  }
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # Enable server timing
+  config.server_timing = true
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join("tmp/caching-dev.txt").exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'your-production-domain.com' } # Remplacez par le domaine de votre application
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } # Définissez l'URL de votre application
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
-    address: 'live.smtp.mailtrap.io',
+    address: 'smtp.gmail.com',
     port: 587,
-    domain: 'mailtrap.club',
-    user_name: ENV.fetch('MAILTRAP_SMTP_USER'),
-    password: ENV.fetch('MAILTRAP_SMTP_PASSWORD'),
+    domain: 'gmail.com',
     authentication: 'plain',
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
   }
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
 
-  # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation = :raise
 
-  # Use a different cache store in production.
-  config.cache_store = :mem_cache_store
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'https://cv-youri-565c941677b0.herokuapp.com/'
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
 
-  # Precompile assets
-  config.assets.compile = false
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # Raises error for missing translations.
+  # config.i18n.raise_on_missing_translations = true
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like NGINX, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :sidekiq
+  # Uncomment if you wish to allow Action Cable access from any origin.
+  # config.action_cable.disable_request_forgery_protection = true
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://your-production-domain.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://your-production-domain.com', /http:\/\/your-production-domain.com.*/ ]
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = true
 end
